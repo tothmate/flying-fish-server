af74f26ba59915fbed3996157f74edf14012f8f2
Akos Toth-Mate <akos@tothmate.com>
2013-01-18 19:03:55 +0100

    low level api added

--- server.py
+++ server.py
@@ -6,6 +6,15 @@ try:
 except:
     print "no serial connection"
 
+def process_command(data):
+    if data in ("L", "R", "U", "D"):
+        ser.write("%s %d\n" % (data, 100))
+    elif data == "S":
+        for i in xrange(0, 10):
+            ser.write("L 80\n")
+            ser.write("R 160\n")
+            ser.write("L 80\n")
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = socket.gethostbyname(socket.gethostname())
 port = 6767
@@ -18,8 +27,7 @@ while True:
     data = client.recv(1)
     if data == '': break
     print "got", data
-    if data in ("L", "R", "U", "D"):
-        if ser is not None:
-            ser.write(data)
+    if ser is not None:
+        process_command(data)
 client.close()
 s.close()
