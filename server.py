a91c5b157862ee0e8f4ca466b781f126a6ac5e00
Akos Toth-Mate <akos@tothmate.com>
2013-01-18 13:42:16 +0100

    simplified socket server

--- server.py
+++ server.py
@@ -1,9 +1,18 @@
-import SocketServer
+import socket 
+from shark import Sharkduino
 
-class FlyingFishHandler(SocketServer.BaseRequestHandler):
-    def handle(self):
-        self.data = self.request.recv(1024).strip()
-        print self.data
+#shark = Sharkduino()
 
-server = SocketServer.TCPServer(("10.0.2.20", 6767), FlyingFishHandler)
-server.serve_forever()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+s.bind(("10.0.2.20", 6767)) 
+s.listen(1)
+client, address = s.accept()
+print "connected"
+while True: 
+    data = client.recv(1).strip()
+    print "got", data
+    if data in ("L", "R", "U", "D"):
+        pass #shark
+    else:
+        break
+client.close()
